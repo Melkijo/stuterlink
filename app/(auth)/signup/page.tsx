db781eccfd497b2f84f7e723c3f73e33cde9f712
melkijo <jonathanandaraa@gmail.com>
2024-05-28 18:04:11 +0800

    fix: signup again

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -30,90 +30,86 @@ function useSearch() {
   return username;
 }
 
-export default function Page() {
+function PageContent() {
   const username = useSearch();
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
+  const form = useForm({
     resolver: zodResolver(formSchema),
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values) {
     signup({ userData: values }, username);
   }
+
+  return (
+    <div className="max-w-[480px] mx-auto mt-20">
+      <p className="mb-4">
+        <span>stuterlink.me/{username}</span> is ready!
+      </p>
+      <h1 className="text-3xl font-bold text-left mb-8">
+        Create your account now
+      </h1>
+      <div className="w-full flex justify-center flex-col items-center">
+        <Button className="w-full">Sign up with Google</Button>
+      </div>
+      <p className="my-4 text-center">Or</p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    required
+                    placeholder="melkijo@example.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    required
+                    placeholder="****"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormDescription>
+            Have an account?{" "}
+            <Link href="/login" className="text-blue-500">
+              Login
+            </Link>
+          </FormDescription>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="max-w-[480px] mx-auto mt-20">
-        <p className="mb-4">
-          <span
-          // className="py-2 px-3 bg-green-500 rounded-lg font-medium"
-          >
-            stuterlink.me/{username}
-          </span>{" "}
-          is ready!
-        </p>
-        <h1 className="text-3xl font-bold text-left mb-8">
-          Create your account now
-        </h1>
-        <div className="w-full flex justify-center flex-col items-center">
-          <Button className="w-full">Sign up with Google</Button>
-        </div>
-        <p className="my-4 text-center">Or</p>
-
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      // name="email"
-                      type="email"
-                      required
-                      placeholder="melkijo@example.com"
-                      {...field}
-                    />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      // name="password"
-                      type="password"
-                      required
-                      placeholder="****"
-                      {...field}
-                    />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormDescription>
-              Have an account?{" "}
-              <Link href="/login" className="text-blue-500">
-                Login
-              </Link>
-            </FormDescription>
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-      </div>
+      <PageContent />
     </Suspense>
   );
 }
