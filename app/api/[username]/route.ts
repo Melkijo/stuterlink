99ecdd2daa2f4a387e502fba0226aa9804a5129c
melkijo <jonathanandaraa@gmail.com>
2024-05-30 00:37:52 +0800

    fix: user GET type data

--- app/api/[username]/route.ts
+++ app/api/[username]/route.ts
@@ -3,14 +3,20 @@ import { NextResponse } from "next/server";
 
 export async function GET( req: Request, context: any){
     const supabase = createClient();
-    const { data: account_data, error} = await supabase.from('account_data').select("*").eq('username', context.params.username)
+    const { data: account_data, error } = await supabase
+        .from('account_data')
+        .select("*")
+        .eq('username', context.params.username);
 
     if (error) {
-      console.error("Error fetching authenticated user:", error);
-      return null;
+        console.error("Error fetching authenticated user:", error);
+        return NextResponse.error();
     }
-    if(account_data){
-        console.log(account_data)
-      return NextResponse.json({account_data})
+
+    if (account_data) {
+        console.log(account_data);
+        return NextResponse.json({ account_data });
     }
+
+    return NextResponse.json({ account_data: [] });
 }
