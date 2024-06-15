1761577bf8cb741c2fc15c4d2f7da12b9fb5be61
melkijo <jonathanandaraa@gmail.com>
2024-06-15 21:54:07 +0800

    fix: auth callback link

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -39,7 +39,7 @@ async function signInWithGoogle() {
   supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: "http://localhost:3000/callback",
+      redirectTo: "https://stuterlink.vercel.app/callback",
     },
   });
 }
